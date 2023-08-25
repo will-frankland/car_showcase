880b6366f8e5037a9873221a8f032209f08cdc0b
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-08-25 10:55:13 -0400

    add new public folder with images and set hero main img

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -15,11 +15,20 @@ const Hero = () => {
         <p className='hero__subtitle'>
           Streamline your car rental experience with our effortless booking process.
         </p>
-        < CustomButton
+        <CustomButton
           title="Explore Cars"
           containerStyles="bg-primary-blue text-white rounded-full mt-10"
           handleClick={handleScroll}
         />
+      </div>
+      <div className='hero__image-container'>
+        <div className='hero__image'>
+          <Image
+            src="/hero.png"
+            alt="hero"
+            fill className='object-contain'
+          />
+        </div>
       </div>
     </div>
   )

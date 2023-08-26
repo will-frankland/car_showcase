e80fc410c764b986cf332e70620c34762c7887c5
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-08-26 10:55:26 -0400

    add form with handleSearch function (empty for now)

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
+"use client";
 
 const SearchBar = () => {
+  const handleSearch = () => {
+
+  };
   return (
-    <div>
-      SearchBar
-    </div>
+    <form className='searchbar' onSubmit={handleSearch}>
+
+    </form>
   )
 }
 
-export default SearchBar
+export default SearchBar;

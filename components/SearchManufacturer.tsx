8ebf4751013d1e6dac4b20af17af3897791b5e70
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-08-26 11:02:17 -0400

    create useState for manufacturer, pass as props and define within types

---
+++ components/SearchManufacturer.tsx
@@ -0,0 +1,12 @@
+import { SearchManfacturerProps } from '@/types'
+import React from 'react'
+
+const SearchManufacturer = ({ manufacturer, setManufacturer } : SearchManfacturerProps) => {
+  return (
+    <div>
+      Search Manufacturer
+    </div>
+  )
+}
+
+export default SearchManufacturer

b2f3a266673ee44a0fafbddf8026b6197e93910e
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-08-26 13:16:59 -0400

    add interface to carDetails and pass down to props

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -80,7 +80,11 @@ const CarCard = ({ car }: CarCardProps) => {
           />
         </div>
       </div>
-      <CarDetails />
+      <CarDetails
+        isOpen={isOpen}
+        closeModal={() => setIsOpen(false)}
+        car={car}
+      />
     </div>
   );
 };

70714caf183bc31b18a04bf715866e83073c5828
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-08-26 11:11:17 -0400

    install headlessUI, add combobox to search component and render car manufacturer logo inside button

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,10 +1,28 @@
+"use client"
+
+import Image from 'next/image';
+
+import { Combobox, Transition } from '@headlessui/react'
 import { SearchManfacturerProps } from '@/types'
 import React from 'react'
 
 const SearchManufacturer = ({ manufacturer, setManufacturer } : SearchManfacturerProps) => {
   return (
-    <div>
-      Search Manufacturer
+    <div className='search-manufacturer'>
+      <Combobox>
+        <div className='relative w-full'>
+          <Combobox.Button className='absolute top-[14px]'>
+            <Image
+              src="/car-logo.svg"
+              width={20}
+              height={20}
+              className="ml-4"
+              alt="Car Logo"
+            />
+
+          </Combobox.Button>
+        </div>
+      </Combobox>
     </div>
   )
 }

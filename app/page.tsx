45b1eaee367de778203a72f7f6aab2ae156fc401
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-08-26 12:21:55 -0400

    add api setup in utils folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,37 @@
-import { Hero, SearchBar, CustomFilter } from '@/components'
-import Image from 'next/image'
+import { Hero, SearchBar, CustomFilter } from "@/components";
+import Image from "next/image";
+import { fetchCars } from "@/utils";
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars();
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
+  console.log(allCars)
+
   return (
     <main className="overflow-hidden">
       <Hero />
-      <div className='mt-12 padding-x padding-y max-width' id='discover'>
-        <div className='home__text-container'>
-          <h1 className='text-4xl font-extrabold'>
-            Car Catalogue
-          </h1>
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
           <p>Explore the perfect cars for you</p>
         </div>
-        <div className='home__filters'>
+        <div className="home__filters">
           <SearchBar />
-          <div className='home__filter-container'>
-            <CustomFilter title='fuel'/>
-            <CustomFilter title='year'/>
+          <div className="home__filter-container">
+            <CustomFilter title="fuel" />
+            <CustomFilter title="year" />
           </div>
         </div>
-
+        {!isDataEmpty ? (
+          <section>WE HAVE CARS</section>
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">Oops, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
     </main>
-  )
+  );
 }

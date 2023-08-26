25d1d037b8f3ed29ff1d11ac99defcc11b24492e
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-08-26 13:06:55 -0400

    add custom btn with extra props, define useState for modal connected to btn

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -12,6 +12,7 @@ interface CarCardProps {
 const CarCard = ({ car }: CarCardProps) => {
   const { city_mpg, year, make, model, transmission, drive } = car;
   const carRent = calculateCarRent(city_mpg, year);
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="car-card group">
       <div className="car-card__content">
@@ -68,6 +69,16 @@ const CarCard = ({ car }: CarCardProps) => {
               {drive.toUpperCase()}
             </p>
           </div>
+        </div>
+        <div className="car-card__btn-container">
+          <CustomButton
+            title="View More"
+            containerStyles="w-full py-[16px] rounded-full bg-primary-blue"
+            textStyles="text-white text-[14px] leading-[17px] font-bold"
+            rightIcon="/right-arrow.svg"
+            handleClick={() => setIsOpen(true)}
+          />
+
         </div>
       </div>
     </div>

e93eed5d9c8c7c1e4713570704e1652ddf80fff1
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-08-28 10:08:27 -0400

    add imports to CarDetails and setup modal background with animation

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -1,4 +1,8 @@
-import React from 'react'
+"use client"
+import Image from 'next/image';
+import { Fragment } from 'react';
+
+import { Dialog, Transition } from '@headlessui/react';
 import { CarProps } from '@/types';
 
 interface CarDetailsProps {
@@ -9,9 +13,25 @@ interface CarDetailsProps {
 
 const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
   return (
-    <div>
-      Car Details
-    </div>
+    <>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10"
+          onClose={closeModal}
+        >
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom='opacity-0'
+            enterTo='opacity-100'
+            leave='ease-in duration-200'
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'
+          >
+            <div className='fixed inset-0 bg-black bg-opacity-25' />
+          </Transition.Child>
+        </Dialog>
+      </Transition>
+    </>
   )
 }
 

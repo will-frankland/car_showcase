032c1f4a565a6d9ec282ef41cca822df274bb012
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-08-28 10:13:11 -0400

    add Transition effects to modal open/close

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -29,6 +29,34 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
           >
             <div className='fixed inset-0 bg-black bg-opacity-25' />
           </Transition.Child>
+          <div className='fixed inset-0 overflow-y-auto'>
+            <div className='flex min-h-full items-enter justify-center p-4 text-center'>
+            <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom='opacity-0 scale-85'
+            enterTo='opacity-100 scale-100'
+            leave='ease-in duration-200'
+            leaveFrom='opacity-100 scale-100'
+            leaveTo='opacity-0 scale-85'
+          >
+            <Dialog.Panel>
+              <button
+                type="button"
+                onClick={closeModal}
+              >
+                <Image
+                  src="/close.svg"
+                  alt="close"
+                  width={20}
+                  height={20}
+                  className='object-contain'
+                />
+              </button>
+            </Dialog.Panel>
+          </Transition.Child>
+            </div>
+          </div>
         </Dialog>
       </Transition>
     </>

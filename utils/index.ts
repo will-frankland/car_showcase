bd711df6f5e80c7b563b9958f68013018c99ff04
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-08-28 10:30:04 -0400

    add and style key/values of car props using Object.entries, modal now populated

--- utils/index.ts
+++ utils/index.ts
@@ -3,7 +3,7 @@ export async function fetchCars() {
 		'X-RapidAPI-Key': process.env.NEXT_PUBLIC_RAPID_API_KEY || "",
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
 	}
-  const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {
+  const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera', {
     headers: headers,
   });
 

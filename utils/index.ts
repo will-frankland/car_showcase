8c1d8981400af34fd100b6fdf04c5f14f6bf4eba
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-08-28 11:21:49 -0400

    add api call via URL with params

--- utils/index.ts
+++ utils/index.ts
@@ -30,5 +30,16 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
 };
 
 export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL('https://cdn.imagin.studio/getimage');
+
+  const { make, year, model } = car;
+
+  url.searchParams.append('customer', 'hrjavascript-mastery');
+  url.searchParams.append('make', make);
+  url.searchParams.append('modelFamily', model.split(' ')[0]);
+  url.searchParams.append('zoomType', 'fullscreen');
+  url.searchParams.append('modelYear', `${year}`);
+  url.searchParams.append('angle', `${angle}`);
   
+  return `${url}`;
 }

932ecc55106ae925b75b37087bf398ecd6c822c2
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-08-29 10:51:20 -0400

    implement search button functionality

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,25 +1,35 @@
 "use client";
-
+import Image from "next/image";
 import { useState } from "react";
 
 import { SearchManufacturer } from "./";
 
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
+  <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+    <Image
+      src="/magnifying-glass.svg"
+      alt="magnifying glass"
+      width={40}
+      height={40}
+      className="object-contain"
+    />
+  </button>
+);
+
 const SearchBar = () => {
   const [manufacturer, setManufacturer] = useState("");
-  const handleSearch = () => {
-
-  };
+  const handleSearch = () => {};
   return (
-    <form className='searchbar' onSubmit={handleSearch}>
+    <form className="searchbar" onSubmit={handleSearch}>
       <div className="searcbar__item">
         <SearchManufacturer
           manufacturer={manufacturer}
           setManufacturer={setManufacturer}
         />
-
+        <SearchButton otherClasses="sm:hidden" />
       </div>
     </form>
-  )
-}
+  );
+};
 
 export default SearchBar;

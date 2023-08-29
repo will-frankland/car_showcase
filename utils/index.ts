0358fae01ae888daa4d5f9bd3752d1a9276ffdac
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-08-29 10:42:08 -0400

    add angle images into modal

--- utils/index.ts
+++ utils/index.ts
@@ -5,7 +5,7 @@ export async function fetchCars() {
 		'X-RapidAPI-Key': process.env.NEXT_PUBLIC_RAPID_API_KEY || "",
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
 	}
-  const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera', {
+  const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=720s', {
     headers: headers,
   });
 

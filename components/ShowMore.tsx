c3d29faaa5da2f7e3a047d1893f01d5b71e78b05
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-08-29 14:04:06 -0400

    impement handleNavigation function and pass to custom button

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -3,12 +3,15 @@ import { ShowMoreProps } from "@/types";
 // Next Imports
 import { useRouter } from "next/navigation";
 import CustomButton from "./CustomButton";
+import { updateSearchParams } from "@/utils";
 
 const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
   const router = useRouter();
 
-  const handleNavigation = () => {
-
+  const handleNavigation = () => {\
+    const newLimit = (pageNumber + 1) * 10;
+    const newPathName = updateSearchParams('limit', `${newLimit}`)
+    router.push(newPathName, {scroll: false});
   };
   return (
     <div className="w-full flex-center gap-5 mt-10">

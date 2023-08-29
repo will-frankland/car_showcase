2b9cd9daa6ac7153cf5ddd5be475cf7550e53afe
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-08-29 13:25:27 -0400

    set props in Custom Filter and define types in types directory

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -7,8 +7,9 @@ import { useRouter } from "next/navigation";
 
 // HeadlessUi Imports
 import { Listbox, Transition } from "@headlessui/react";
+import { CustomFilterProps } from "@/types";
 
-const CustomFilter = () => {
+const CustomFilter = ({ title, options }: CustomFilterProps) => {
   return (
     <div>
       Custom Filter

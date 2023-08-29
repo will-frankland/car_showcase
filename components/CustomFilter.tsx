902177cf9e6e33a2de061c8bb4678e8e067bc1e3
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-08-29 13:28:26 -0400

    create useState for filter selection and pass to Listbox added to render

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -10,9 +10,19 @@ import { Listbox, Transition } from "@headlessui/react";
 import { CustomFilterProps } from "@/types";
 
 const CustomFilter = ({ title, options }: CustomFilterProps) => {
+  const [selected, setSelected] = useState(options[0]);
   return (
-    <div>
-      Custom Filter
+    <div className="w-fit">
+      <Listbox
+        value={selected}
+        onChange={(e) => setSelected(e)}
+      >
+        <div className="relative w-fit z-10">
+          <Listbox.Button className="custom-filter__btn">
+            <span>Filter</span>
+          </Listbox.Button>
+        </div>
+      </Listbox>
     </div>
   )
 }

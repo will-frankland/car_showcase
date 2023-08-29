2b1ae8c00d6aad876dfff759489942b9f61ac96a
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-08-29 13:34:24 -0400

    add fuel and year filters with styling and functionality

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -19,8 +19,37 @@ const CustomFilter = ({ title, options }: CustomFilterProps) => {
       >
         <div className="relative w-fit z-10">
           <Listbox.Button className="custom-filter__btn">
-            <span>Filter</span>
+            <span className="block truncate">{selected.title}</span>
+            <Image
+              src="/chevron-up-down.svg"
+              width={20}
+              height={20}
+              className="ml-4 object-contain"
+              alt="chevron up down"
+            />
           </Listbox.Button>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Listbox.Options className="custom-filter__options">
+              {options.map((option) => (
+                <Listbox.Option
+                  key={option.title}
+                  value={option}
+                  className={({ active }) => `relative cursor-default select-none py-2 px-4 ${active ? 'bg-primary-blue text-white' : 'text-gray-900'}`}
+                >
+                  {({ selected }) => (
+                    <span>
+                      {option.title}
+                    </span>
+                  )}
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </Transition>
         </div>
       </Listbox>
     </div>

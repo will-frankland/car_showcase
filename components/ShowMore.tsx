c5277aec721749e705ff583b4c77023f5f3fa09a
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-08-29 14:12:14 -0400

    final changes

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -8,7 +8,7 @@ import { updateSearchParams } from "@/utils";
 const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
   const router = useRouter();
 
-  const handleNavigation = () => {\
+  const handleNavigation = () => {
     const newLimit = (pageNumber + 1) * 10;
     const newPathName = updateSearchParams('limit', `${newLimit}`)
     router.push(newPathName, {scroll: false});

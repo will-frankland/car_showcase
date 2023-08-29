3c3f7f9d2d77ecfecee15509ce80daea4c39d62c
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-08-29 13:52:20 -0400

    move search funtion to utlis, import and utilize for fuel and year search

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { useState, Fragment } from "react";
 
 // Next Imports
@@ -8,15 +8,21 @@ import { useRouter } from "next/navigation";
 // HeadlessUi Imports
 import { Listbox, Transition } from "@headlessui/react";
 import { CustomFilterProps } from "@/types";
+import { updateSearchParams } from "@/utils";
 
 const CustomFilter = ({ title, options }: CustomFilterProps) => {
+  const router = useRouter();
   const [selected, setSelected] = useState(options[0]);
+
+  const handleUpdateParams = (e: { title: string, value: string }) => {
+    const newPathName = updateSearchParams(title, e.value.toLowerCase());
+
+    router.push(newPathName);
+  };
+
   return (
     <div className="w-fit">
-      <Listbox
-        value={selected}
-        onChange={(e) => setSelected(e)}
-      >
+      <Listbox value={selected} onChange={(e) => { setSelected(e); handleUpdateParams(e)}}>
         <div className="relative w-fit z-10">
           <Listbox.Button className="custom-filter__btn">
             <span className="block truncate">{selected.title}</span>
@@ -39,10 +45,18 @@ const CustomFilter = ({ title, options }: CustomFilterProps) => {
                 <Listbox.Option
                   key={option.title}
                   value={option}
-                  className={({ active }) => `relative cursor-default select-none py-2 px-4 ${active ? 'bg-primary-blue text-white' : 'text-gray-900'}`}
+                  className={({ active }) =>
+                    `relative cursor-default select-none py-2 px-4 ${
+                      active ? "bg-primary-blue text-white" : "text-gray-900"
+                    }`
+                  }
                 >
                   {({ selected }) => (
-                    <span className={`block truncate ${selected ? 'font-medium' : 'font-normal'}`}>
+                    <span
+                      className={`block truncate ${
+                        selected ? "font-medium" : "font-normal"
+                      }`}
+                    >
                       {option.title}
                     </span>
                   )}
@@ -53,7 +67,7 @@ const CustomFilter = ({ title, options }: CustomFilterProps) => {
         </div>
       </Listbox>
     </div>
-  )
-}
+  );
+};
 
-export default CustomFilter
+export default CustomFilter;
